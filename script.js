6573f825d451db4c2d4439e8a6b5e6b835a92a7e
Ubuntu <ubuntu@ip-172-31-17-155.ec2.internal>
2024-05-16 05:02:54 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,5 @@
+hello how are you all 
+what are you doing here today whate you all learin
+hello iam going to learn from my mistakes todayonwards
+to learn the english everyone is facing the problems
+who has to study hard those will overcome thoe problems
